248eeda2ab23355eb89b4425fc858c645cdb3c48
MarkoMarkovikjEndava <marko.markovikj@endava.com>
2022-11-08 14:32:49 +0100

    more changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
 	return (
 		<Router>
 			<Routes>
-				<Route path="/" element={<Seniors />} />
+				<Route path="/kumanovo-formacii" element={<Seniors />} />
 			</Routes>
 		</Router>
 	);
